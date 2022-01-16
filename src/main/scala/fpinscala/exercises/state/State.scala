393207b42112dc83b104aa1b730fa6b073b3c57b
Thomas Ploch <profiploch@gmail.com>
2022-01-16 10:11:36 +0100

    State intDouble,doubleInt,double3 exercise

--- src/main/scala/fpinscala/exercises/state/State.scala
+++ src/main/scala/fpinscala/exercises/state/State.scala
@@ -1,5 +1,7 @@
 package fpinscala.exercises.state
 
+import scala.annotation.tailrec
+
 
 trait RNG:
   def nextInt: (Int, RNG) // Should generate a random `Int`. We'll later define other functions in terms of `nextInt`.
@@ -35,11 +37,20 @@ object RNG:
     val (i, next) = nonNegativeInt(rng)
     (i / (Int.MaxValue.toDouble + 1), next)
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int,Double), RNG) =
+    val (i, next1) = rng.nextInt
+    val (d, next2) = double(next1)
+    ((i, d), next2)
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double,Int), RNG) =
+    val ((i, d), next) = intDouble(rng)
+    ((d, i), next)
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def double3(rng: RNG): ((Double,Double,Double), RNG) =
+    val (d1, next1) = double(rng)
+    val (d2, next2) = double(next1)
+    val (d3, next3) = double(next2)
+    ((d1, d2, d3), next3)
 
   def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
 
